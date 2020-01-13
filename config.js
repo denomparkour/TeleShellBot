599f04eca178216e4f49207a638e14213085792c
Denomparkour <42404684+denomparkour@users.noreply.github.com>
2020-01-13 09:42:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     config:function(){
         return (
             {
-                adminUsers: [464848593, 401643149, 300522174], //admin users' telegram id, should be numbers
-                botToken: '973281407:AAEDs2NEgXIHjO1SVQfcVl2BftrH5x5UVmw', // bot token
+                adminUsers: [879820429], //admin users' telegram id, should be numbers
+                botToken: '646885536:AAEKFxy_Xxd9_7yQ59zeaPkipgun-a-r8vw', // bot token
 
             }
         );
